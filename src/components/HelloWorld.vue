bb28b5dec010891efcb5b6710325f312f24d5ec6
fc836731493 <AntonyQQ998>
2019-07-15 21:03:10 +0800

    第一次上次

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div>
+      <ul>
+        <li v-for="news in newsData" >
+           <p>{{ news.screen_name }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     newsData:[]
+    }
+  },
+  created(){
+      this.$axios("https://www.apiopen.top/satinApi")
+      .then(res => {
+         this.newsData = res.data;
+         console.log(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
+
+<style >
+
+</style>
